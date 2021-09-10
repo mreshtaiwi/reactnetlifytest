63f8bedb60c04e48e9bb9b4a72ed82f6a5c5836f
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-09-10 23:58:13 +0300

    fixing

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -7,7 +7,7 @@ class Parent extends React.Component {
         return (
             <>
                 <h3>
-                    parent Component
+                    this is parent Component
                 </h3>
                 <Cat catName='koko' catImage='https://media.wired.com/photos/5cdefb92b86e041493d389df/191:100/w_1280,c_limit/Culture-Grumpy-Cat-487386121.jpg' />
                 <Cat catName='fofo' catImage={fofoImage} />
